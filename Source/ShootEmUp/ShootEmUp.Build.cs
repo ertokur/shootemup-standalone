17b6dbbf0d3f61b4f2d43feac402e1281a385a48
Erbol Tokurov <erbol.tokur@gmail.com>
2025-12-11 04:11:31 +0600

    [config]: Included Niagara module to project

--- Source/ShootEmUp/ShootEmUp.Build.cs
+++ Source/ShootEmUp/ShootEmUp.Build.cs
@@ -8,7 +8,7 @@ public class ShootEmUp : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Niagara" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Niagara", "PhysicsCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
