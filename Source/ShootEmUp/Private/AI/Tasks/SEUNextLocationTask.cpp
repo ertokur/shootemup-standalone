94cc2e5b64ac3e387337badb9ff9658412d0fcb5
Erbol Tokur <erbol.tokur@gmail.com>
2026-09-01 04:44:26 +0600

    [feat]: Added behavior tree task

---
+++ Source/ShootEmUp/Private/AI/Tasks/SEUNextLocationTask.cpp
@@ -0,0 +1,54 @@
+﻿// Erbol Tokur
+
+
+#include "AI/Tasks/SEUNextLocationTask.h"
+#include "AIController.h"
+#include "NavigationSystem.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+
+USEUNextLocationTask::USEUNextLocationTask()
+{
+	NodeName = "Select Next Location";
+}
+
+EBTNodeResult::Type USEUNextLocationTask::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	const TObjectPtr<AAIController> Controller = OwnerComp.GetAIOwner();
+	const TObjectPtr<UBlackboardComponent> Blackboard = OwnerComp.GetBlackboardComponent();
+	
+	if (!Controller || !Blackboard)
+		return EBTNodeResult::Failed;
+	
+	const TObjectPtr<APawn> Pawn = Controller->GetPawn();
+	
+	if (!Pawn)
+		return EBTNodeResult::Failed;
+	
+	const TObjectPtr<UNavigationSystemV1> NavSystem = UNavigationSystemV1::GetCurrent(Pawn);
+	
+	if (!NavSystem)
+		return EBTNodeResult::Failed;
+	
+	FNavLocation ResultLocation;
+	FVector Location = Pawn->GetActorLocation();
+	
+	if (!SelfCenter)
+	{
+		AActor* CenterActor = Cast<AActor>(Blackboard->GetValueAsObject(CenterActorKey.SelectedKeyName));
+		
+		if (!CenterActor)
+			return EBTNodeResult::Failed;
+		
+		Location = CenterActor->GetActorLocation();
+	}
+	
+	const bool bFound = NavSystem->GetRandomReachablePointInRadius(Location, Radius, ResultLocation);
+	
+	if (!bFound)
+		return EBTNodeResult::Failed;
+	
+	Blackboard->SetValueAsVector(LocationKey.SelectedKeyName, ResultLocation.Location);
+	
+	return EBTNodeResult::Succeeded;
+}
---
+++ Source/ShootEmUp/Public/AI/Tasks/SEUNextLocationTask.h
@@ -0,0 +1,34 @@
+﻿// Erbol Tokur
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "SEUNextLocationTask.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTEMUP_API USEUNextLocationTask : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+public:
+	USEUNextLocationTask();
+	
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Radius = 1000.f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FBlackboardKeySelector LocationKey;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool SelfCenter = true;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (EditCondition = "!SelfCenter"))
+	FBlackboardKeySelector CenterActorKey;
+};
