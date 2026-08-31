ed432f0b3c2bba6f2b1a1ef066774ca6dd35f5a4
Erbol Tokur <erbol.tokur@gmail.com>
2026-09-01 04:42:10 +0600

    [feat]: Added AICharacter & AIController

---
+++ Source/ShootEmUp/Private/AI/SEUAIController.cpp
@@ -0,0 +1,28 @@
+﻿// Erbol Tokur
+
+#include "AI/SEUAIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Components/SEUAIPerceptionComponent.h"
+
+ASEUAIController::ASEUAIController()
+{
+	AIPerceptionComponent = CreateDefaultSubobject<USEUAIPerceptionComponent>("PerceptionComponent");
+	SetPerceptionComponent(*AIPerceptionComponent);
+}
+
+void ASEUAIController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+}
+
+void ASEUAIController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	SetFocus(GetFocusOnActor());
+}
+
+AActor* ASEUAIController::GetFocusOnActor() const
+{
+	if (!GetBlackboardComponent()) return nullptr;
+	return Cast<AActor>(GetBlackboardComponent()->GetValueAsObject(FocusOnKeyName));
+}
---
+++ Source/ShootEmUp/Public/AI/SEUAIController.h
@@ -0,0 +1,31 @@
+// Erbol Tokur
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "SEUAIController.generated.h"
+
+class USEUAIPerceptionComponent;
+
+UCLASS()
+class SHOOTEMUP_API ASEUAIController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	ASEUAIController();
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	USEUAIPerceptionComponent* AIPerceptionComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI")
+	FName FocusOnKeyName = "EnemyActor";
+
+	virtual void OnPossess(APawn* InPawn) override;
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	AActor* GetFocusOnActor() const;
+};
