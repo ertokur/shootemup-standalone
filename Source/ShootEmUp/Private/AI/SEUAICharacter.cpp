1a966580b67a0bb37d347441b560cd8187db8cd9
Erbol Tokur <erbol.tokur@gmail.com>
2026-09-10 03:32:17 +0600

    [feat]: Added SEUAICharacter

---
+++ Source/ShootEmUp/Private/AI/SEUAICharacter.cpp
@@ -0,0 +1,56 @@
+﻿// Erbol Tokur
+
+
+#include "AI/SEUAICharacter.h"
+
+#include "BrainComponent.h"
+#include "AI/SEUAIController.h"
+#include "Components/SEUAIWeaponComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+
+// Sets default values
+ASEUAICharacter::ASEUAICharacter(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer.SetDefaultSubobjectClass<USEUAIWeaponComponent>("WeaponComponent"))
+{
+	AIControllerClass = ASEUAIController::StaticClass();
+	
+	bUseControllerRotationYaw = false;
+	if (GetCharacterMovement())
+	{
+		GetCharacterMovement()->bUseControllerDesiredRotation = true;
+		GetCharacterMovement()->RotationRate = FRotator(0.0f, 200.0f, 0.0f);
+	}
+}
+
+// Called when the game starts or when spawned
+void ASEUAICharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ASEUAICharacter::OnDeath()
+{
+	Super::OnDeath();
+
+	const auto AIController = Cast<AAIController>(Controller);
+	if (AIController && AIController->BrainComponent)
+	{
+		AIController->BrainComponent->Cleanup();
+	}
+	
+}
+
+// Called every frame
+void ASEUAICharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ASEUAICharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/ShootEmUp/Public/AI/SEUAICharacter.h
@@ -0,0 +1,30 @@
+﻿// Erbol Tokur
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Core/SEUCharacterBase.h"
+#include "SEUAICharacter.generated.h"
+
+UCLASS()
+class SHOOTEMUP_API ASEUAICharacter : public ASEUCharacterBase
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ASEUAICharacter(const FObjectInitializer& ObjectInitializer);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	virtual void OnDeath() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
